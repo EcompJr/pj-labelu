f153145b45dd50f109bb9a366bbf477484bf8072
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-05-22 19:54:48 -0300

    Install TailwindCSS v4 (#12)
    * Install TailwindCSS v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,34 +5,42 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	// Deploying to GitHub Pages under the repository subpath
-	site: 'https://ecompjr.github.io',
-	base: '/pj-labelu/',
-	integrations: [icon(), mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
+  // Deploying to GitHub Pages under the repository subpath
+  site: 'https://ecompjr.github.io',
+
+  base: '/pj-labelu/',
+  integrations: [icon(), mdx(), sitemap()],
+
+  fonts: [
+      {
+          provider: fontProviders.local(),
+          name: 'Atkinson',
+          cssVariable: '--font-atkinson',
+          fallbacks: ['sans-serif'],
+          options: {
+              variants: [
+                  {
+                      src: ['./src/assets/fonts/atkinson-regular.woff'],
+                      weight: 400,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+                  {
+                      src: ['./src/assets/fonts/atkinson-bold.woff'],
+                      weight: 700,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+              ],
+          },
+      },
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
