f41d1dc3e976e3dc366b1f6acf107dc4d18a4710
Ítallo Guimarães <italloguimaraes1@gmail.com>
2026-05-23 00:58:44 -0300

    feat: cria pagina de acesso ao acervo, integra ao cms e adiciona mock data

--- src/content.config.ts
+++ src/content.config.ts
@@ -45,4 +45,13 @@ const agenda = defineCollection({
 	}),
 });
 
-export const collections = { blog, pesquisadores, agenda };
+const acervo = defineCollection({
+    // Load Markdown and MDX files in the `src/content/acervo/` directory.
+    loader: glob({ base: './src/content/acervo', pattern: '**/*.{md,mdx}' }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+    }),
+});
+
+export const collections = { blog, pesquisadores, agenda, acervo };
