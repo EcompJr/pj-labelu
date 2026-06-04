e3ba34420886ef8bd92deb989ccefd2207e66f47
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-06-04 18:03:13 -0300

    Enhance SEO

--- src/consts.ts
+++ src/consts.ts
@@ -2,8 +2,11 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const REPO = "pj-labelu"
-export const SITE_TITLE = 'Labelu'
-export const SITE_DESCRIPTION = "Laboratório de História e Memória da Luta de Classes"
-export const COPYRIGHT_NAME = "Labelu - Laboratório de História e Memória da Luta de Classes"
+export const SITE_TITLE = 'Labelu ✦ Laboratório de História e Memória da Luta de Classes'
+export const SITE_DESCRIPTION = `
+Somos, o nome diz, um laboratório — um espaço para o exercício de um trabalho (labor) intelectual: a pesquisa histórica. Existimos há mais de 21 anos fazendo pesquisa e ajudando a formar bons pesquisadores em História.
+Nosso objetivo principal é pesquisar a história e preservar a memória das lutas sociais e da esquerda. Nossos estudos dialogam intensamente com a História Social, investigando sujeitos, estratégias, memórias e os antagonismos que se defrontam nas lutas de classes.
+`
+export const COPYRIGHT_NAME = "Labelu — Laboratório de História e Memória da Luta de Classes"
 export const LOCATION = "Prédio do Programa de Pós-graduação em História, Anexo do Módulo 7 - UEFS. Av. Transnordestina, s/n - Novo Horizonte, Feira de Santana - BA, 44036-900."
 export const CONTACT_EMAIL = "uefslabelu@gmail.com"
